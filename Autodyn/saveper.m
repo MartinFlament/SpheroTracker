b9547b3a1307e462f98613279e5dcb3b766bb3d6
MartinFlament <martin.flament39@gmail.com>
2025-11-28 16:49:36 +0100

    Delete Autodyn directory

--- Autodyn/saveper.m
+++
@@ -1,2 +0,0 @@
-function saveper(PathName,FileName,PS)
-   save([PathName FileName(1:end-12) '_period.mat'], 'PS');
--- Autodyn/test.m
+++
@@ -1,9 +0,0 @@
-for ii=1:length(results)
-    mm(ii)=max(results(ii).sp(:,2));
-    titi(ii)=results(ii).pts2(100,1);
-end
-
-figure
-plot(mm)
-figure
-plot(titi)
